22eaebab7f4d70708c4924ecea8b56b624c7eed5
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-07-21 21:43:49 +0530

    Create Exp5-B-Consonant-or-Vowel.cpp

---
+++ Exp5-B-Consonant-or-Vowel.cpp
@@ -0,0 +1,19 @@
+// Rajendra Chaganti
+// PRN : 24070123150 
+// B3
+#include <iostream>
+using namespace std;
+int main() {
+    char ch;
+    // Take input from user
+    cout << "Enter a character: ";
+    cin >> ch;
+    // Check if the inputted character is vowel or consonant
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        cout << "It is a vowel." << endl;
+    } else {
+        cout << "It is a consonant." << endl;
+    }
+    return 0;
+}
