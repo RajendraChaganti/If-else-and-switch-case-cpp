0d200dba1997f356deb8ab2e757a042d96b0dbfa
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-07-21 22:03:16 +0530

    Create  Exp5-D-Switch-Case.cpp

---
+++ Exp5-D-Switch-Case.cpp
@@ -0,0 +1,31 @@
+// Rajendra Chaganti
+// PRN : 24070123150
+// B3
+#include <iostream>
+using namespace std;
+int main()
+{
+    int choice;
+    cout << "Enter any number in between 1 to 5 to choose for any outdoor local game:";
+    cin >> choice;
+    switch(choice) {
+        case 1:
+            cout << "Lagoori" << endl;
+            break;
+        case 2:
+            cout << "Kho Kho" << endl;
+            break;
+        case 3:
+            cout << "Gilli Danda" << endl;
+            break;
+        case 4:
+            cout << "Kabbadi"<<endl;
+            break;
+        case 5:
+            cout << "Pakdam Pakdai" << endl;
+            break;
+        default:
+            cout << "Ice Water" << endl;
+    }
+    return 0;
+}
