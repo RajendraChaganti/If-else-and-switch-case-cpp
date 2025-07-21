97f6d9f521a8f3f587866b15c0668ce00bbf8cf1
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-07-21 21:38:33 +0530

    Update Exp-5-A-EvenorOdd.cpp

--- Exp-5-A-EvenorOdd.cpp
+++ Exp-5-A-EvenorOdd.cpp
@@ -1,1 +1,19 @@
+// Rajendra Chaganti
+// PRN : 24070123150
+// B3
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    // Input from user
+    cout << "Enter a number: ";
+    cin >> number;
+    // Check if the number is even or odd using if-else
+    if (number % 2 == 0) {
+        cout << number << " is even." << endl;
+    } else {
+        cout << number << " is odd." << endl;
+    }
+    return 0;
+}
 
